4d64ca2e307b64867ca0c388c78566dde89e9bf2
nickluymes <ksemyul@gmail.com>
2024-08-29 21:32:31 -0400

    Updates to the home range models - switch to elevation instead of snow for buffer models

--- scripts/01_download_datafiles.R
+++ scripts/01_download_datafiles.R
@@ -10,16 +10,26 @@
 
 ### Create folders for data if they don't exist
 dir.create("data/raw/landcover", showWarnings = FALSE)
-dir.create("data/raw/nwt_names", showWarnings = FALSE)
+dir.create("data/raw/geography", showWarnings = FALSE)
+dir.create("data/raw/MRDEM", showWarnings = FALSE)
 
 ### download 2010 Land Cover of Canada .tif
 tif_url <- "https://datacube-prod-data-public.s3.ca-central-1.amazonaws.com/store/land/landcover/landcover-2010-classification.tif"
 options(timeout = max(2000, getOption("timeout")))
 download.file(tif_url, "data/raw/landcover/landcover-2010-classification.tif", mode = "wb")
 
-### download metadata
+### download landcover metadata
 meta_url <- "https://open.canada.ca/data/en/dataset/c688b87f-e85f-4842-b0e1-a8f79ebf1133.xml"
 download.file(meta_url, "data/raw/landcover/landcover-2010-classification.tif2.xml")
+
+### download Medium Resolution Digital Elevation Model .tif
+dem_url <- "https://datacube-prod-data-public.s3.ca-central-1.amazonaws.com/store/elevation/mrdem/mrdem-30/mrdem-30-dtm.tif"
+options(timeout = max(2000, getOption("timeout")))
+download.file(dem_url, "data/raw/MRDEM/mrdtm.tif", mode = "wb")
+
+### download DEM metadata
+dem_meta_url <- "https://open.canada.ca/data/en/dataset/18752265-bda3-498c-a4ba-9dfe68cb98da.xml"
+download.file(dem_meta_url, "data/raw/MRDEM/mrdtm.tif.xml")
 
 ### download NWT place names
 names_url <- "https://ftp.cartes.canada.ca/pub/nrcan_rncan/vector/geobase_cgn_toponyme/prov_shp_eng/cgn_nt_shp_eng.zip"
